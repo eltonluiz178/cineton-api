b5daf765cd58b9f025c503af4b54e7c446fff1cd
eltonluiz178 <eltonluiz533@gmail.com>
2026-07-08 00:23:27 -0300

    chore: initial project setup via Spring Initializr

---
+++ src/main/java/dev/cineton/CinetonApiApplication.java
@@ -0,0 +1,13 @@
+package dev.cineton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinetonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinetonApiApplication.class, args);
+	}
+
+}
