5c744e1b0928ca283b66a57e3a768cfcb18ae487
eltonluiz178 <eltonluiz533@gmail.com>
2026-07-08 22:13:31 -0300

    feat: cria migration para povoar o admin

---
+++ src/main/resources/db/migration/V5__Seed_admin_user.sql
@@ -0,0 +1,28 @@
+-- ====================================================================================================
+-- Migration : V5__Seed_admin_user.sql
+-- Projeto   : Cineton
+-- Objetivo  : Criação do usuário administrador inicial.
+-- ====================================================================================================
+
+INSERT INTO users (
+    id,
+    first_name,
+    last_name,
+    email,
+    password,
+    role,
+    created_at,
+    updated_at
+)
+VALUES (
+           gen_random_uuid(),
+           'System',
+           'Administrator',
+           'admin@cineton.com',
+           '$2a$10$M9z9Hf8nQJ7w2x4v0QKq7.2X6cA5Y8xYx4n4XQ7M5tO3A1QK8r6kG',
+           'ADMIN',
+           NOW(),
+           NOW()
+       )
+    ON CONFLICT (email)
+DO NOTHING;
