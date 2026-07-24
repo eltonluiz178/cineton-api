a8141f3c905be3f0cd9e6eb7462f836f726be7f9
eltonluiz178 <eltonluiz533@gmail.com>
2026-07-23 21:15:13 -0300

    feat: adiciona a lista de gêneros no retorno do filme

--- src/main/java/dev/cineton/dto/response/FilmResponse.java
+++ src/main/java/dev/cineton/dto/response/FilmResponse.java
@@ -4,6 +4,7 @@ import dev.cineton.domain.entities.Film;
 import dev.cineton.domain.enums.FilmStatus;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public record FilmResponse(
@@ -15,9 +16,10 @@ public record FilmResponse(
         LocalDate releaseDate,
         String posterUrl,
         String trailerUrl,
-        FilmStatus status
+        FilmStatus status,
+        List<GenreResponse> genres
 ) {
     public FilmResponse(Film film) {
-        this(film.getId(), film.getTitle(), film.getSynopsis(), film.getDurationMinutes(), film.getAgeRating(), film.getReleaseDate(), film.getPosterUrl(), film.getTrailerUrl(), film.getStatus());
+        this(film.getId(), film.getTitle(), film.getSynopsis(), film.getDurationMinutes(), film.getAgeRating(), film.getReleaseDate(), film.getPosterUrl(), film.getTrailerUrl(), film.getStatus(), film.getGenres().stream().map(GenreResponse::new).toList());
     }
 }
