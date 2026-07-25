3025015796af94923fb09e0127cf4a588d992420
eltonluiz178 <eltonluiz533@gmail.com>
2026-07-25 00:32:14 -0300

    feat: adiciona método para upload do poster do filme e método para ajuste de resposta do filme dinâmicamente se tem poster ou não

--- src/main/java/dev/cineton/service/FilmService.java
+++ src/main/java/dev/cineton/service/FilmService.java
@@ -3,6 +3,7 @@ package dev.cineton.service;
 import dev.cineton.dto.request.CreateFilmRequest;
 import dev.cineton.dto.request.UpdateFilmRequest;
 import dev.cineton.dto.response.FilmResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,4 +14,5 @@ public interface FilmService {
     List<FilmResponse> findAll();
     FilmResponse update(UUID id, UpdateFilmRequest request);
     void delete(UUID id);
+    FilmResponse uploadPoster(UUID id, MultipartFile file);
 }
