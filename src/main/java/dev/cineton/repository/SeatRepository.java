f30415417ba5adc3e166125f40a97c68ab79ecb3
eltonluiz178 <eltonluiz533@gmail.com>
2026-08-04 23:30:26 -0300

    feat: adiciona repositório para assentos

---
+++ src/main/java/dev/cineton/repository/SeatRepository.java
@@ -0,0 +1,13 @@
+package dev.cineton.repository;
+
+import dev.cineton.domain.entities.Room;
+import dev.cineton.domain.entities.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SeatRepository extends JpaRepository<Seat, UUID> {
+    List<Seat> findByRoom(Room room);
+    void deleteByRoom(Room room);
+}
