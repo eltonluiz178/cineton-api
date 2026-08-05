a7fb72b2f0ac5d4ffc16d710df3311b5e0733520
eltonluiz178 <eltonluiz533@gmail.com>
2026-08-04 23:30:03 -0300

    fix: ajusta método de comparação do nome para sala

--- src/main/java/dev/cineton/repository/RoomRepository.java
+++ src/main/java/dev/cineton/repository/RoomRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface RoomRepository extends JpaRepository<Room, UUID> {
-    boolean findByName (String name);
+    boolean existsByName (String name);
 }
